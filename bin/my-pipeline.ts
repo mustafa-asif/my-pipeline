3ec7b7572aa2ad9bfa968feba3c2c619dfb44270
mustafa <mustafawarind03@gmail.com>
2026-01-28 20:09:03 +0500

    initial commit

---
+++ bin/my-pipeline.ts
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import * as cdk from 'aws-cdk-lib/core';
+import { MyPipelineStack } from '../lib/my-pipeline-stack';
+
+const app = new cdk.App();
+new MyPipelineStack(app, 'MyPipelineStack', {
+  env: { account: '763701915116', region: 'ap-south-1' }
+ 
+});
+app.synth();
---
+++ lib/my-pipeline-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib/core';
+import { Construct } from 'constructs';
+import { CodePipeline,CodePipelineSource,ShellStep  } from 'aws-cdk-lib/pipelines';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class MyPipelineStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const pipeline = new CodePipeline(this, 'MyPipeline', {
+      pipelineName: 'MyServicePipeline',
+      synth: new ShellStep('Synth', {
+        input: CodePipelineSource.connection(
+          'mustafa-asif/my-pipline', 'main',
+          {
+            connectionArn:'arn:aws:codeconnections:ap-south-1:763701915116:connection/eefbdaf7-3fd7-421c-bc6e-37602deba6fd'
+          }
+        ),
+        commands: [
+          'npm ci',
+          'npm run build',
+          'npx cdk synth'
+        ]
+      })
+    });
+  }
+}
