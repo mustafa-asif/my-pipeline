a8ce38850935d36cfac093571ebddc8fbe24eed9
mustafa <mustafawarind03@gmail.com>
2026-02-02 19:00:20 +0500

    configure stage stack inint

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib/core';
 import { Construct } from 'constructs';
 import { CodePipeline,CodePipelineSource,ShellStep  } from 'aws-cdk-lib/pipelines';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { MyPipelineAppStage } from './my-pipeline-app-stage';
 
 export class MyPipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -26,5 +27,10 @@ export class MyPipelineStack extends cdk.Stack {
         ]
       })
     });
+
+    pipeline.addStage(new MyPipelineAppStage(this, 'test',{
+      env: { account: '763701915116', region: 'ap-south-1' }
+    }));
+
   }
 }
